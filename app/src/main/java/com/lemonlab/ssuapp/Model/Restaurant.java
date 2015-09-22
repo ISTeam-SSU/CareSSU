28851cfa6bc80d3a2936d2bf8a45727ccf4d8480
yangporo <529yhs@gmail.com>
2015-09-23 06:44:48 +0900

    mamuri

---
+++ app/src/main/java/com/lemonlab/ssuapp/Model/Restaurant.java
@@ -0,0 +1,36 @@
+package com.lemonlab.ssuapp.Model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by heechan on 15. 9. 23..
+ */
+public class Restaurant {
+    String title;
+    Drawable image;
+    boolean isLiked;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public void setIsLiked(boolean isLiked) {
+        this.isLiked = isLiked;
+    }
+}
---
+++ app/src/main/java/com/lemonlab/ssuapp/View/RestaurantFragment.java
@@ -0,0 +1,54 @@
+package com.lemonlab.ssuapp.View;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lemonlab.ssuapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RestaurantFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RestaurantFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RestaurantFragment extends Fragment {
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RestaurantFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RestaurantFragment newInstance(String param1, String param2) {
+        RestaurantFragment fragment = new RestaurantFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RestaurantFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant, container, false);
+    }
+
+}
